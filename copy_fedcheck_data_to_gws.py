5dc6bb9f920e5d28ae105a3da4b8c942ac582273
Ruth Petrie <rpetrie@jasmin-sci1.ceda.ac.uk>
2017-01-26 16:08:55 +0000

    Update, still not dealing with latest correctly

--- copy_fedcheck_data_to_gws.py
+++ copy_fedcheck_data_to_gws.py
@@ -50,7 +50,7 @@ def copy_files(source_basedir, dest_basedir):
 
     split_basedir = [part.strip() for part in source_basedir.split('/')]
     project, product, institute, model, experiment, frequency = split_basedir[4:10]
-
+    project = 'cmip6'
     for path, dirs, files in os.walk(source_basedir):
         for file in files:
             if not os.path.islink(os.path.join(path, file)):
@@ -62,8 +62,15 @@ def copy_files(source_basedir, dest_basedir):
                 realm, table, ensemble, folder, var_date, ncfile = parse_filename(os.path.join(path, file))
 
                 # Name destination file in esgf-fedcheck gws files/ folder
+                dest_dir = os.path.join(dest_basedir, project, product, institute, model, experiment,
+                                             frequency, realm, table, ensemble, var_date, folder)
+                if not os.path.exists(dest_dir):
+                    os.makedirs(dest_dir)
+
                 dest_file = os.path.join(dest_basedir, project, product, institute, model, experiment,
                                              frequency, realm, table, ensemble, var_date, folder, ncfile)
+
+                shutil.copy(source_file, dest_file)
 
                 print "COPY:" + source_file
                 print "DEST:" + dest_file
@@ -79,57 +86,52 @@ def copy_files(source_basedir, dest_basedir):
 
                 if not os.path.exists(dest_version_dir):
                     print "MAKE DIR: " + dest_version_dir
-                    # os.makedirs(dest_version_dir)
+                    os.makedirs(dest_version_dir)
 
+                # CHANGE DIR
+                print "PWD: " + dest_version_dir
+                os.chdir(dest_version_dir)
 
                 # file to symlink to
-                symblink_file = os.path.join(dest_version_dir, ncfile)
-                # os.symlink(symblink_file, dest_file)
+#                link_target = os.path.join('../files/d%/s%') % (version, ncfile)
+                link_target = os.path.join('../files/' + version + '/' + ncfile)
+                link_src = os.path.join(dest_version_dir, ncfile)
+                # MAKE LINK
+                print "LINKING: " + link_target
+                print "WITH   : " + link_src
+                os.symlink(link_target, link_src)
 
+                # CREATE "latest" DIRECTORY SYMLINK
 
-                # LATEST
                 # Make latest dir if it doesn't exist
                 latest_dir = os.path.join(dest_basedir, project, product, institute, model, experiment,
                                              frequency, realm, table, ensemble, varid, 'latest')
 
                 if not os.path.exists(latest_dir):
-                    print "MAKE DIR: " + latest_dir
-                    # os.makedirs(latest_dir)
 
-                # Populate the version folder with symlinks to the data
-
-                # DECIDE ON WHICH IS LATEST
-                latest_file = os.path.join(latest_dir, ncfile)
-
-                if not os.path.isfile(os.path.join(latest_dir, ncfile)):
-
+                    print "SYMLINK LATEST: " + dest_version_dir
+                    print "WITH    LATEST: " + latest_dir
+                    os.symlink(dest_version_dir, latest_dir)
+                else:
+                    # DECIDE ON WHICH IS LATEST
                     # get current version
+                    existing_dir_date = os.readlink(latest_dir)[-8:]
+                    date_ints = [int(item) for item in (existing_dir_date[:4], existing_dir_date[4:6], existing_dir_date[6:8])]
+                    existing_version = datetime.datetime(*date_ints)
+
                     date_ints = [int(item) for item in (date[:4], date[4:6], date[6:8])]
                     current_version = datetime.datetime(*date_ints)
 
-                    # get symlink version
-#                    symlink_file = os.path.realpath(latest_file)
 
-                    dest_file_var = dest_file.split('/')[16]
-                    dest_file_year = int(dest_file_var.split('_')[1].strip()[:4])
-                    dest_file_mon = int(dest_file_var.split('_')[1].strip()[4:6])
-                    dest_file_day = int(dest_file_var.split('_')[1].strip()[6:])
-                    symlink_version = datetime.datetime(dest_file_year, dest_file_mon, dest_file_day)
-
-                    if current_version > symlink_version:
-                        print "REPLACING SYMLINK SOURCE: " + latest_file
-                        print "REPLACING SYMLINK DESTIN: " + dest_file
-                        # os.symlink(latest_file, dest_file)
-                else:
-                    print "MAKE LATEST SYMLINK SOURCE: " + latest_file
-                    print "MAKE LATEST SYMLINK DESTIN: " + dest_file
-                    # make symbolic links
-                    # os.symlink(latest_file, dest_file)
-
+                    if current_version > existing_version:
+                        # REPLACE SYMLINK
+                        print "REPLACING SYMLINK SOURCE: " + dest_version_dir
+                        print "REPLACING SYMLINK DESTIN: " + latest_dir
+                        os.symlink(dest_version_dir, latest_dir)
 
 if __name__ == "__main__":
 
     source_basedir = '/badc/cmip5/data/cmip5/output1/MOHC/HadGEM2-ES/esmControl/mon'
-    dest_basedir = '/group_workspaces/jasmin/esgf_fedcheck/archive/cmip5/data'
+    dest_basedir = '/group_workspaces/jasmin/esgf_fedcheck/archive/badc/cmip6/data'
     dest_file = copy_files(source_basedir, dest_basedir)
 
